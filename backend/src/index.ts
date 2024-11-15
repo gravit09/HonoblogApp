8822b7bff79b864594ad9d0a24e0fc14a30dc48f
Gravit09 <garvitchoudhary09@gmail.com>
2024-11-15 15:19:52 +0530

    added more routes

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -3,8 +3,9 @@ import { decode, sign, verify } from "hono/jwt";
 import { PrismaClient } from "@prisma/client/edge";
 import { withAccelerate } from "@prisma/extension-accelerate";
 import { empty } from "@prisma/client/runtime/library";
+import { use } from "hono/jsx";
 
-const app = new Hono<{ Bindings: Bindings }>();
+const app = new Hono<{ Bindings: Bindings; Variables: Variables }>();
 
 app.get("/", (c) => {
   return c.text("Hello Hono!");
@@ -14,6 +15,23 @@ interface Bindings {
   DATABASE_URL: string;
   SECRET: string;
 }
+
+interface Variables {
+  userId: any;
+}
+
+//middleware in hono also can use the function way to write middleware
+app.use("/api/v1/blog/*", async (c, next) => {
+  const header = c.req.header("Authorization") || "";
+  const token = header.split(" ")[1];
+  const response = await verify(token, c.env.SECRET);
+  if (!response) {
+    c.status(403);
+    return c.json("Unauthorized");
+  }
+  c.set("userId", response.id);
+  await next();
+});
 
 app.post("/api/v1/signup", async (c) => {
   const prisma = new PrismaClient({
@@ -45,6 +63,30 @@ app.post("/api/v1/signup", async (c) => {
     message: "Signup Succesfull",
     jwt: token,
   });
+});
+
+app.post("/api/v1/signin", async (c) => {
+  const prisma = new PrismaClient({
+    datasourceUrl: c.env.DATABASE_URL,
+  }).$extends(withAccelerate());
+  const body = await c.req.json();
+
+  const user = await prisma.user.findUnique({
+    where: {
+      email: body.email,
+      password: body.password,
+    },
+  });
+
+  if (!user) {
+    c.status(403);
+    return c.json("No user with this credentials found");
+  }
+
+  const payload = { name: user.name, id: user.id };
+  const token = await sign(payload, c.env.SECRET);
+  c.status(200);
+  return c.json({ message: "Login Success", token });
 });
 
 export default app;
